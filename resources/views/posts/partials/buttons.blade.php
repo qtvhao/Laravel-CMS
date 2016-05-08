fe729af68fc7500cc0ce6d00f42e5f7f63e3cb31
Nghiem Xuan Hao <qtvhao@gmail.com>
2016-05-08 16:41:46 +0700

    fix buttons of article section

--- resources/views/posts/partials/buttons.blade.php
+++ resources/views/posts/partials/buttons.blade.php
@@ -1,13 +1,15 @@
 	<div>
 	@if (isset($is_in_loop) and $is_in_loop)
-		<a href='{{ route('posts.show', $post->id) }}' class="btn btn-default">View Post</a>
+		<a href='{{ route('posts.show', $post->id) }}' class="btn btn-default">
+		<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+		View Post</a>
 	@endif
 	<a href='{{ route('posts.create') }}' class="btn btn-default">
-		<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+		<span class="glyphicon glyphicon-plus" aria-hidden="true"></span> New
 	</a>
 @can('edit', $post)
     <a href='{{ route('posts.edit', $post->id) }}' class="btn btn-default">
-		<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+		<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Edit
 	</a>
 @endcan
 	</div>
